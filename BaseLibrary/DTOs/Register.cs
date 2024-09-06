7a0f72f1782b2b86257444990d95bc9a2fcf94c5
vitheshshetty00 <vitheshshetty23@gmail.com>
2024-09-06 14:22:00 +0530

    feat: Add database entities and establish database connection
    - Created all necessary entities for the database
    - Configured and connected the database to the application

---
+++ BaseLibrary/DTOs/Register.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+    public class Register :AccountBase
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
+        public string? Fullname { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string? ConfirmPassword { get; set; }
+
+    }
+}
---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServerLibrary.Data
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<GeneralDepartment> GeneralDepartment { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<SystemRole> SystemRoles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
